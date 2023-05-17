6992276119ea60762c2e495ce33c1e60b9d51c32
Klara Sejnova <klarasejnova@mezzanine.local>
2023-05-17 20:12:52 +0200

    crochet setup

---
+++ React/crochet/src/containers/Header.js
@@ -0,0 +1,9 @@
+import "../containerstyles/header.scss";
+function Header() {
+  return (
+    <div className="header-container">
+      <h1>Crochet friends</h1>
+    </div>
+  );
+}
+export default Header;
