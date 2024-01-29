c9c225672cdc3129198eb128249958d20f7bc6a1
Klara Sejnova <klarasejnova@mezzanine.local>
2024-01-29 11:11:47 +0100

    icons into components

--- timetable-react/src/components/daysContainer.component.jsx
+++ timetable-react/src/components/daysContainer.component.jsx
@@ -3,7 +3,7 @@ import DayTuesdayItem from "./dayTuesday.component";
 
 const DaysContainerItem = () => {
   return (
-    <div className="days flex-column gap-7">
+    <div className="days flex-column gap-7 border border-black-100 rounded-lg p-20 shadow-xl">
       <DayMondayItem></DayMondayItem>
       <DayTuesdayItem></DayTuesdayItem>
     </div>
